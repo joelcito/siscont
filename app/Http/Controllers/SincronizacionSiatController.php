4c1aeffa728a10bb54fef2c1e461a8979ab77301
JOEL FLORES QUISPE <jjjoelcito123@gmail.com>
2026-02-07 23:27:42 -0400

    se

--- app/Http/Controllers/SincronizacionSiatController.php
+++ app/Http/Controllers/SincronizacionSiatController.php
@@ -162,7 +162,7 @@ class SincronizacionSiatController extends Controller
                 $cuis->codigo,
                 $empresa->nit
             ));
-            dd($sincronizarParametricaTipoDocumentoIdentidad);
+            // dd($sincronizarParametricaTipoDocumentoIdentidad);
             if($sincronizarParametricaTipoDocumentoIdentidad->resultado->RespuestaListaParametricas){
                 $array = $sincronizarParametricaTipoDocumentoIdentidad->resultado->RespuestaListaParametricas->listaCodigos;
                 foreach ($array as $key => $value) {
