bac86f4e55129269a3c300a619f20549b6f83504
JOEL FLORES QUISPE <jjjoelcito123@gmail.com>
2026-02-07 23:23:18 -0400

    se

--- app/Http/Controllers/SiatController.php
+++ app/Http/Controllers/SiatController.php
@@ -320,6 +320,7 @@ class SiatController extends Controller
             $resultado = false;
             $data['estado'] = 'error';
             $data['resultado'] = $resultado;
+            $data['mensaje'] = $fault;
         }
         return json_encode($data, JSON_UNESCAPED_UNICODE);
     }
